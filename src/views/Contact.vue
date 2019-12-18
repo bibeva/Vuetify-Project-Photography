959c7c8e7dd030f1ccd5c6be8a9d01b9d4dcad46
unknown <bibekneva@gmail.com>
2019-12-18 08:20:51 +0000

    work on contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,11 +9,67 @@
         </v-col>
       </v-row>
     </v-img>
+    <div class="block">
+      <v-container>
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          <v-select
+            v-model="select"
+            :items="items"
+            :rules="[v => !!v || 'Item is required']"
+            label="Item"
+            required
+          ></v-select>
+
+          <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
+
+          <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
+
+          <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+        </v-form>
+      </v-container>
+    </div>
   </v-content>
 </template>
 
 <script>
 export default {
-  name: 'Contact'
-}
+  name: "Contact",
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    ],
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    select: null,
+    items: [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4',
+    ],
+    checkbox: false,
+  }),
+
+  methods: {
+    validate () {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+      }
+    },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    },
+  }
+};
 </script>
