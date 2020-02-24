08bc1112b9cc5ae759c3e94cb86bd6d01aa4c89b
Bibek <bibekneva@gmail.com>
2020-02-24 14:56:54 +0000

    working on Not Found page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,11 +1,23 @@
 <template>
   <v-content>
-    Not Found
+    <v-container>
+      <div class="block text-center notFound">
+        <h2>Page not found</h2>
+        <i class="fas fa-exclamation-triangle red--text"></i>
+        <p>We're sorry, the page you requested could not be found. Please go back to the homepage or contact us at admin@bibekshakya.com</p>
+      </div>
+    </v-container>
   </v-content>
 </template>
 
 <script>
 export default {
-  name: 'NotFound'
-}
+  name: "NotFound",
+
+  data() {
+    return {
+      items: []
+    };
+  }
+};
 </script>
